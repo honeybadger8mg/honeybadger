b78eae0312ea10721747274190c34639f56eff0f
aaa <aaa@qqq.com>
2023-01-21 17:08:02 +0800

    删除注释

--- examples/AndroidDemo/nativelib1/src/main/java/com/honeybadger/nativelib/NativeLib1.java
+++ examples/AndroidDemo/nativelib1/src/main/java/com/honeybadger/nativelib/NativeLib1.java
@@ -1,16 +1,11 @@
 package com.honeybadger.nativelib;
 
 public class NativeLib1 {
-
-    // Used to load the 'nativelib' library on application startup.
     static {
         System.loadLibrary("nativelib1");
     }
 
-    /**
-     * A native method that is implemented by the 'nativelib' native library,
-     * which is packaged with this application.
-     */
     public static native String DoTest1(String strArg);
+
     public static native int DoTest2(int iArg);
 }
