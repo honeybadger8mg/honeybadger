fd8ad3d0056749dc205e7ead7191f38fa3548d54
aaa <aaa@qqq.com>
2023-01-22 15:37:39 +0800

    添加反射测试代码

--- examples/AndroidDemo/nativelib1/src/main/cpp/nativelib1.cpp
+++ examples/AndroidDemo/nativelib1/src/main/cpp/nativelib1.cpp
@@ -3,7 +3,7 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_honeybadger_nativelib_NativeLib1_DoTest1(
-        JNIEnv* env,
+        JNIEnv *env,
         jclass, jstring jsArg) {
     const char *szData = env->GetStringUTFChars(jsArg, JNI_FALSE);
     std::string strData = szData;
@@ -17,4 +17,23 @@ JNIEXPORT jint JNICALL
 Java_com_honeybadger_nativelib_NativeLib1_DoTest2(JNIEnv *env, jclass clazz, jint i_arg) {
     int iOut = i_arg + 100;
     return iOut;
+}
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_honeybadger_nativelib_NativeLib1_DoTestReflect(JNIEnv *env, jclass clazz) {
+    jclass jcTarget = env->FindClass("com/honeybadger/nativelib/NativeLib1");
+    if (jcTarget == nullptr) {
+        return JNI_FALSE;
+    }
+    jmethodID mtdTarget = env->GetStaticMethodID(jcTarget, "TestReflect",
+                                                 "(Ljava/lang/String;)Ljava/lang/String;");
+    if (mtdTarget == nullptr) {
+        return JNI_FALSE;
+    }
+    jstring jsArg = env->NewStringUTF("来自于native的参数");
+    jstring jsResult = (jstring) env->CallStaticObjectMethod(jcTarget, mtdTarget, jsArg);
+    const char *szResult = env->GetStringUTFChars(jsResult, JNI_FALSE);
+    bool bVerify = strcmp(szResult, "ok");
+    env->ReleaseStringUTFChars(jsArg, szResult);
+    return bVerify;
 }
--- examples/AndroidDemo/nativelib1/src/main/java/com/honeybadger/nativelib/NativeLib1.java
+++ examples/AndroidDemo/nativelib1/src/main/java/com/honeybadger/nativelib/NativeLib1.java
@@ -1,5 +1,7 @@
 package com.honeybadger.nativelib;
 
+import android.util.Log;
+
 public class NativeLib1 {
     static {
         System.loadLibrary("nativelib1");
@@ -8,4 +10,12 @@ public class NativeLib1 {
     public static native String DoTest1(String strArg);
 
     public static native int DoTest2(int iArg);
+
+    // 测试反射
+    public static String TestReflect(String strArg) {
+        Log.d("NativeLib1", "TestReflect 函数调用  参数是:" + strArg);
+        return "ok";
+    }
+    public static native boolean DoTestReflect();
+
 }
